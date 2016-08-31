7e74db44b93396f2996af4585a4180c3f8b197ad
Vitor <valencastrol@gmail.com>
2016-08-31 08:21:15 -0300

    Histogramas das Features

--- Histogramas/histogramasFeatures.m
+++ Histogramas/histogramasFeatures.m
@@ -1,5 +1,79 @@
-carros = dir('../Imagens/TreinamentSecoes/Carros/*.png');
-vagas = dir('../Imagens/TreinamentSecoes/Vagas/*.png');
+% carro1 = imread('carroVermelho.png');
+% carro2 = imread('carroPreto.png');
+% vaga1 = imread('Nada1.png');
+% vaga2 = imread('Nada2.png');
+% 
+% 
+% largura = 16;
+% altura = 16;
+% 
+% cbVaga2 = [];
+% crVaga2 = [];
+% contrasteVaga2 = [];
+% correlacaoVaga2 = [];
+% energiaVaga2 = [];
+% homoVaga2 = [];
+% 
+% img = vaga2;
+% 
+% 
+% for i = 1:largura:size(img,1)-largura-1
+%    for j = 1:altura:size(img,2)-altura-1
+%        sGrey = img(i:i+largura-1, j:j+altura-1);
+%        sRgb = img(i:i+largura-1, j:j+altura-1,1:3);
+%        sYCbCr = rgb2ycbcr(sRgb);
+%        cbVaga2 = [cbVaga2, mean(mean(sYCbCr(:,:,2)))];
+%        crVaga2 = [crVaga2, mean(mean(sYCbCr(:,:,3)))];
+%        glcm = graycomatrix(sGrey);
+%        stats = graycoprops(glcm, 'all');
+%        contrasteVaga2 = [contrasteVaga2, stats.Contrast];
+%        correlacaoVaga2 = [correlacaoVaga2, stats.Correlation];
+%        energiaVaga2 = [energiaVaga2, stats.Energy];
+%        homoVaga2 = [homoVaga2, stats.Homogeneity];
+%    end
+% end
+
+figure(1)
+[n,c] = hist(double(homoCarro1));
+bar(c,n,'EdgeColor', 'r', 'FaceColor','none');
+hold on
+[n,c] = hist(double(homoCarro2));
+bar(c,n,'EdgeColor', 'g', 'FaceColor','none');
+hold on
+[n,c] = hist(double(homoVaga1));
+bar(c,n,'EdgeColor', 'b', 'FaceColor','none');
+hold on
+[n,c] = hist(double(homoVaga2));
+bar(c,n,'EdgeColor', 'y', 'FaceColor','none');
+hold on
+
+
+% enderecoCarros = 'C:\Users\Avell G1711 NEW\Documents\MATLAB\Imagens\SecoesColoridas\Carros\*.png';
+% enderecoVagas = 'C:\Users\Avell G1711 NEW\Documents\MATLAB\Imagens\SecoesColoridas\Vagas\*.png';
+% 
+% carros = dir(enderecoCarros);
+% vagas = dir(enderecoVagas);
+% 
+% cbVagas = [];
+% crVagas = [];
+% 
+% for i=1:length(vagas)
+%     img = imread(vagas(i).name);
+%     img = rgb2ycbcr(img);
+%     cbVagas = [cbVagas, mean(mean(img(:,:,2)))];
+%     crVagas = [crVagas, mean(mean(img(:,:,3)))];
+% end
+% 
+% 
+% figure(1)
+% [n,c] = hist(crCarros);
+% bar(c,n,'EdgeColor', 'b', 'FaceColor','none');
+% hold on
+% [n,c] = hist(crVagas);
+% bar(c,n,'EdgeColor', 'r', 'FaceColor','none');
+% hold off
+
+
 
 
 
---
+++ Histogramas/pegaImagens.m
@@ -0,0 +1,24 @@
+img = imread('MeioCheio2.png');
+imshow(img);
+rect1 = getrect;
+rect1 = floor(rect1);
+
+largura = 50;
+altura = rect1(4);
+
+x = rect1(1);
+y = rect1(2);
+
+cont = 65;
+
+while(x  < (rect1(1) + rect1(3)) - largura)
+    
+    s = img(y:y+altura,x:x+largura,1:3);
+    imwrite(s,['secao',int2str(cont), '.png']);
+    
+     x = x + largura;
+     cont = cont +1;
+end
+
+
+
