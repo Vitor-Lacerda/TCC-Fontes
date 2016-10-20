6fcf1164947a01ebf24b585c11f2d17d9393dc05
Vitor <valencastrol@gmail.com>
2016-10-20 18:29:27 -0200

    imgCinza agora é o canal Y

--- Principal/extraiCaracteristicasSecoes.m
+++ Principal/extraiCaracteristicasSecoes.m
@@ -1,7 +1,7 @@
 function colunaAtributos = extraiCaracteristicasSecoes(img)
 
 imgYCbCr = rgb2ycbcr(img);
-imgCinza = rgb2gray(img);
+imgCinza = imgYCbCr(:,:,1);
 glcm = graycomatrix(imgCinza);
 features = graycoprops(glcm,'all');
 
